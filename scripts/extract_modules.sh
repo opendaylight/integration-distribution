f081d38802ea0170989828a7dfa76e4e73c9e35b
Robert Varga <robert.varga@pantheon.tech>
2018-06-28 14:11:24 +0200

    Add module extraction script
    This is a first, raw, cut at extracting models out an opendaylight
    distribution.
    
    Change-Id: I92c03634d660cc56ea20a81104e0bf10addb7526
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ scripts/extract_modules.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+#
+
+#
+# This script visits all jars within the OpenDaylight karaf distribution and extracts
+# all production YANG modules (as located in META-INF/yang)
+#
+set -e
+
+# FIXME: make this tuneable
+OUTPUT="opendaylight-models"
+INPUT="."
+
+JARS=`find $INPUT/system/org/opendaylight -type f -name '*.jar' | sort -u`
+
+# FIXME: also wipe output?
+[ -d "$OUTPUT" ] || mkdir "$OUTPUT"
+for jar in $JARS; do
+    artifact=`basename $jar | sed 's/.jar$//'`
+    # FIXME: better control over unzip errors
+    unzip -q "$jar" 'META-INF/yang/*' -d "$artifact" \
+        2>/dev/null || true
+
+    dir="$artifact/META-INF/yang"
+    if [ -d "$dir" ]; then
+        for file in `find $dir -type f -name '*.yang'`; do
+            module=`basename "$file"`
+            echo "Extracting $module from $artifact"
+            mv -n "$file" "$OUTPUT"
+        done
+    fi
+
+    rm -rf "$artifact"
+done
+
+
