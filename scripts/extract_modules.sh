ada9b1e6c67f68f597196924e23cc5bb91b5f255
Dhiraj240 <dhirajcoolssharma@gmail.com>
2019-06-06 18:56:27 +0530

    Made changes to integration/distribution/extract_modules.sh
    Signed-off-by: Dhiraj Sharma <dhirajcoolssharma@gmail.com>
    Change-Id: Ic51d5a6babb6399b4107ef51a0b8ec2d66c637df

--- scripts/extract_modules.sh
+++ scripts/extract_modules.sh
@@ -17,12 +17,12 @@ set -e
 OUTPUT="opendaylight-models"
 INPUT="."
 
-JARS=`find $INPUT/system/org/opendaylight -type f -name '*.jar' | sort -u`
+JARS=$(find $INPUT/system/org/opendaylight -type f -name '*.jar' | sort -u)
 
 # FIXME: also wipe output?
 [ -d "$OUTPUT" ] || mkdir "$OUTPUT"
 for jar in $JARS; do
-    artifact=`basename $jar | sed 's/.jar$//'`
+    artifact=$(basename "$jar" | sed 's/.jar$//')
     echo "Extracting modules from $artifact"
     # FIXME: better control over unzip errors
     unzip -q "$jar" 'META-INF/yang/*' -d "$artifact" \
@@ -30,8 +30,8 @@ for jar in $JARS; do
 
     dir="$artifact/META-INF/yang"
     if [ -d "$dir" ]; then
-        for file in `find $dir -type f -name '*.yang'`; do
-            module=`basename "$file"`
+        for file in $(find "$dir" -type f -name '*.yang'); do
+            module=$(basename "$file")
             echo -e "\t$module"
             # FIXME: better duplicate detection
             mv -n "$file" "$OUTPUT"
