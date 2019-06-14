707f7c76dd21aae24dfed537f7cb859d5b7a0877
Rohan Julka <rohanjulka19@gmail.com>
2019-06-14 12:17:20 +0530

    Minor fixes in extract_modules script
    Resolved some fixme's in the script.The yang models which are extracted
    from the extract_modules script are now organized in different folders on
    the basis of the ODL project it belongs to. Filtered yang models not
    belonging to opendaylight on the basis of namespace field inisde yang
    models.The script can now be invoked from any folder.
    
    Change-Id: Ie51155f6aa83410a89d2c7b4d00ea5c635065f5c
    Signed-off-by: Rohan Julka <rohanjulka19@gmail.com>

---
+++ karaf-scripts/src/main/assembly/bin/extract_modules.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#
+# Copyright (c) 2018 Pantheon Technologies, s.r.o. and others.  All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License v1.0 which accompanies this distribution,
+# and is available at http://www.eclipse.org/legal/epl-v10.html
+#
+
+#
+# This script visits all jars within the OpenDaylight karaf distribution and extracts
+# all production YANG modules (as located in META-INF/yang)
+#
+set -e
+BIN_DIR=`dirname $0`
+OUTPUT="$BIN_DIR/../opendaylight-models"
+INPUT="$BIN_DIR/.."
+
+# FIXME: also wipe output?
+[ -d "$OUTPUT" ] || mkdir "$OUTPUT"
+
+PROJECTS=`ls -d $INPUT/system/org/opendaylight/*`
+for proj in $PROJECTS; do
+    proj=`basename $proj`
+    echo "Extracting modules from $proj"
+    JARS=`find $INPUT/system/org/opendaylight/$proj -type f -name '*.jar' | sort -u`
+    [ -d "$OUTPUT/$proj" ] || mkdir $OUTPUT/$proj
+    for jar in $JARS; do
+        unzip -l "$jar" | grep -q -e "\.yang$" &&
+        unzip -q "$jar" 'META-INF/yang/*' -d "$OUTPUT/$proj"
+    done
+    if [ -z "$(ls -A $OUTPUT/$proj)" ]; then
+        rm -rf $OUTPUT/$proj
+    else
+        rm -f $OUTPUT/$proj/*.yang
+        YANGS=`find $OUTPUT/$proj/META-INF/yang/ -type f -name '*.yang'`
+        for yang in $YANGS; do
+            name=`basename $yang`
+            less $yang | grep -q namespace.*opendaylight &&
+            mv $OUTPUT/$proj/META-INF/yang/$name $OUTPUT/$proj &&
+            echo "  $name"
+        done
+        rm -rf $OUTPUT/$proj/META-INF
+    fi
+done
+echo "Models are extracted to ${OUTPUT//"bin/../"/}"
