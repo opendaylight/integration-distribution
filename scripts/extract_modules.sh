158c27b17ab242cf870e8e67ad30d2adf761ba73
Rohan Julka <rohanjulka19@gmail.com>
2019-06-14 12:17:20 +0530

    Organized the yang modules by ODL sub-projects
    The yang models which are extracted from the extract_modules script are
    now organized in different folders on the basis of the ODL project it
    belongs to .
    
    Change-Id: Ie51155f6aa83410a89d2c7b4d00ea5c635065f5c
    Signed-off-by: Rohan Julka <rohanjulka19@gmail.com>

--- scripts/extract_modules.sh
+++ scripts/extract_modules.sh
@@ -17,27 +17,40 @@ set -e
 OUTPUT="opendaylight-models"
 INPUT="."
 
-JARS=`find $INPUT/system/org/opendaylight -type f -name '*.jar' | sort -u`
-
 # FIXME: also wipe output?
 [ -d "$OUTPUT" ] || mkdir "$OUTPUT"
-for jar in $JARS; do
-    artifact=`basename $jar | sed 's/.jar$//'`
-    echo "Extracting modules from $artifact"
-    # FIXME: better control over unzip errors
-    unzip -q "$jar" 'META-INF/yang/*' -d "$artifact" \
+
+PROJECTS=`ls -d $INPUT/system/org/opendaylight/*`
+for proj in $PROJECTS; do 
+    proj=`basename $proj`
+    echo "Extracting modules from $proj"
+    JARS=`find $INPUT/system/org/opendaylight/$proj -type f -name '*.jar' | sort -u`
+    cd $OUTPUT
+    [ -d "$proj" ] || mkdir $proj 
+    cd - > /dev/null
+    for jar in $JARS; do
+        artifact=`basename $jar | sed 's/.jar$//'`
+        # FIXME: better control over unzip errors
+        unzip -q "$jar" 'META-INF/yang/*' -d "$artifact" \
         2>/dev/null || true
 
-    dir="$artifact/META-INF/yang"
-    if [ -d "$dir" ]; then
-        for file in `find $dir -type f -name '*.yang'`; do
-            module=`basename "$file"`
-            echo -e "\t$module"
-            # FIXME: better duplicate detection
-            mv -n "$file" "$OUTPUT"
-        done
+        dir="$artifact/META-INF/yang"
+        if [ -d "$dir" ]; then
+            for file in `find $dir -type f -name '*.yang'`; do
+                module=`basename "$file"`
+                echo -e "\t$module"
+                # FIXME: better duplicate detection
+                mv -n "$file" "$OUTPUT/$proj"
+            done
+        fi
+
+        rm -rf "$artifact"
+    done
+    pushd $INPUT > /dev/null
+    cd $OUTPUT/$proj
+    if [ -z "$(ls -A)" ]; then
+        cd ..
+        rmdir $proj
     fi
-
-    rm -rf "$artifact"
+    popd > /dev/null
 done
-
